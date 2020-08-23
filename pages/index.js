1df21a4ed331fc56b06926d079fe0a7e8a074d15
Alexandre <alexandre.omph@gmail.com>
2020-08-23 05:41:34 -0300

    feat: Pages and navigation

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import Link from 'next/link'
+
 const Home = () => (
-  <h1>Hello World</h1>
+  <>
+    <h1>Hello World</h1>
+    <Link href="users">
+      <a>
+        Usuarios
+      </a>
+    </Link>
+  </>
 )
 
 export default Home;
