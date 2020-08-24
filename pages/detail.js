500236acbbba406cdb43435f0a181740fbf55764
Alexandre <alexandre.omph@gmail.com>
2020-08-24 02:03:22 -0300

    feat: custom server

---
+++ pages/detail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import axios from "axios";
+const User = ({ user }) => (
+  <div>
+    <h1>User: {user.login}</h1>
+    <img src={user.avatar_url} width="200" />
+  </div>
+);
+
+User.getInitialProps = async ({ query }) => {
+  const response = await axios.get(
+    `https://api.github.com/users/${query.user}`
+  );
+
+  return { user: response.data };
+};
+
+export default User
---
+++ routes.js
@@ -0,0 +1,7 @@
+
+const routes = require('next-routes')
+
+module.exports = routes()
+  .add('/', 'home')
+  .add('/users', 'users')
+  .add('/users/:user', 'detail')
