1433e9f595bc4e9c38b9ab6679c251b629a5066b
Alexandre <alexandre.omph@gmail.com>
2020-08-24 01:48:02 -0300

    feat: statics resources

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ const Home = () => (
     <Head>
       <title>Home</title>
     </Head>
+    <img src="/static/panda.jpg" width="200" alt="" />
     <h1>Hello World</h1>
     <Link href="users">
       <a>
